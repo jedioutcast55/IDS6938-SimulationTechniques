6225140d5a5c990cc5e0e4ba24b204c7b18a0ad8
Roberto Cabrera <roberto.cabrera@knigths.ucf.edu>
2017-03-25 05:01:49 -0400

    Working with Discrete to create a matrix.

--- Discrete3/main.cpp
+++ Discrete3/main.cpp
@@ -6,14 +6,69 @@
 
 int main(){
   //SIMULATE DISCRETE TIME MARKOV CHAIN
-  std::vector< std::vector<double> > matrix(3,std::vector<double> (3)); //initializes a 3x3 matrix with zeros
+  std::vector< std::vector<double> > matrix(11,std::vector<double> (11)); //initializes a 11x11 matrix with zeros
 
   //set transition matrix
-  matrix[0][1] = 1;
-  matrix[1][2] = 1;
-  matrix[2][0] = 1;
+  for (int i = 0; i < 11; i++)
+  {
+	  for (int j = 0; j < 11; j++)
+	  {
+		  matrix[i][j] = 0.0;
+	  }
+  }
 
-  int steps = 10;
+  //set transition matrix
+  int j = 0;
+  int k =11;
+  int countIter = 0;
+  double pIncrease = (2.0 / 6.0);
+  for (int i = 0; i < k; i++)
+  {
+	  j = i + 1;
+
+	  
+	  countIter = 0;
+
+	  // We only need to do it six times - six rolls.
+	  for (j; j < k; j++)
+	  {
+		  if (countIter < 7)
+		  {
+			  matrix[i][j] = (1.0 / 6.0);
+			  countIter++;
+		  }
+
+
+		  if (i >= (k - 6) && (j == k-1))
+		  {
+			  matrix[i][j] = pIncrease;
+			  pIncrease = pIncrease + (1.0 / 6.0);
+		  }
+	  }
+
+	
+  }
+
+  // Last state by itself
+  matrix[k - 1][k - 1] = 1;
+
+  for (int m = 0; m < 11; m++)
+  {
+	  std::cout << "This is state: " << m << std::endl;
+
+	  for (int n = 0; n < 11; n++)
+	  {
+		  std::cout << matrix[m][n] << " ";
+	  }
+
+	  std::cout << std::endl;
+  }
+
+  std::cout << std::endl;
+
+
+
+  int steps = 100;
   int start = 0;
   double T = 8.5;
   
